36ed625f23a2945b9b057579139338e3c70c9149
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-05 07:56:33 -0800

    Created card component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Robofriends</h1>
-      </header>
+      <Card />
     </div>
   );
 }

9bdd4eaf356fb58e8902e77770cd0247dfea5ae9
Fabian <100328289+FabianUlisesCarrillo@users.noreply.github.com>
2022-12-05 08:37:08 -0800

    Created searchbar component to search for robot

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,7 +3,7 @@ import Card from './Card';
 
 const CardList = ({ robots }) => {
     const cardComponent = robots.map((robot, i) => {
-        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} key={i}/>
     })
     return(
         <div>
---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <div className='pa2'>
+      <input type="text" placeholder='search for robot' className='pa3 ba b--green bg-lightest-blue'/>
+    </div>
+  )
+}
+
+export default SearchBar
